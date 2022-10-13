7cafb91bfaa8442f4b3adf6174cdcbd5c6e40d2a
Karlessandra Lima <karlessandralima@gmail.com>
2022-10-12 23:04:19 -0300

    first commit

---
+++ components/Form/Button/Button.tsx
@@ -0,0 +1,31 @@
+import React, {ButtonHTMLAttributes} from 'react'
+import styled from '@emotion/styled';
+
+const ButtonData = styled.button`
+    border: none;
+    border-radius: 10px;
+    height: 30px;
+    margin-left: 5px;
+    color: #333;
+    background: linear-gradient(150deg, #ffeb84 0% #ffd53e 100%);
+    box-shadow: 0px 2px 5px -1px rgb(59, 59, 59, 0.5);
+
+    &:disabled {
+        background: #ccc;
+        color: #666;
+        cursor: not-allowed;
+    }
+`;
+
+type ButtonProps = {
+    children: React.ReactNode;
+}
+// Union Types 
+type ButtonUnion = ButtonProps | ButtonHTMLAttributes<HTMLButtonElement>;
+
+
+export default function Button(props:ButtonUnion) {
+  return (
+    <ButtonData {...props}>{props.children}</ButtonData>
+  )
+}
---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function LoginPage() {
+  return (
+    <>
+    <Header pesquisar='Buscar no site'/>
+    <h1>Seja bem vindo ao React</h1>
+    </>
+  )
+}
+
